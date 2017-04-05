b629578bd4b8c882d4ad2cfc91152fa79159b993
Gladyshev <Gladyshev@GLADYSHEV>
2017-04-05 23:07:35 +0300

    BasisFolder

---
+++ SimpleBlank/Interface/IDataElement.cs
@@ -0,0 +1,19 @@
+﻿using SimpleBlank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlank.Interface
+{
+    interface IDataElement
+    {
+        string FullName { get; set; }
+        string Name { get; set; }
+
+        State State { get; set; }
+
+
+    }
+}
---
+++ SimpleBlank/Models/DataElement.cs
@@ -0,0 +1,34 @@
+﻿using Mvvm.Core.ViewModels;
+using SimpleBlank.Interface;
+
+namespace SimpleBlank.Models
+{
+    public abstract class DataElement : ViewModelBase, IDataElement
+    {
+        public string FullName
+        {
+            get { return _fullName; }
+
+            set { UpdateValue(value, ref _fullName); }
+        }
+
+        public string Name
+        {
+            get { return _fullName; }
+
+            set { UpdateValue(value, ref _name); }
+        }
+
+        public State State
+        {
+            get { return _state; }
+
+            set
+            { UpdateValue(value, ref _state); }
+        }
+
+        private string _name;
+        private string _fullName;
+        private State _state;
+    }
+}
